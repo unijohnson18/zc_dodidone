bebab6c4ec43265eee788dc9b66f213e25cc28f3
unijohnson18 <unijohnson@gmail.com>
2024-07-24 16:34:28 +0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/zc_dodidone/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.zc_dodidone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:zc_dodidone/app/app.dart';
+
+void main() {
+  runApp(const MyApp());
+}
